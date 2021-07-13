3741ec04a94577933c878dc3fd19e17359f6b72f
fleven <franz.leven@kickstartds.com>
2021-07-13 14:40:10 +0200

    feat: add teaser variants

--- src/Seiten/Landingpage.story.jsx
+++ src/Seiten/Landingpage.story.jsx
@@ -162,7 +162,7 @@ const Page = () => (
         pageHeader: false,
         spaceAfter: 'none'
       }}
-      background="accent"
+      background="dark"
       mode="tile"
       spaceAfter="default"
       spaceBefore="default"
@@ -202,18 +202,83 @@ const Page = () => (
     >
       <TeaserBox
         darkStyle= "true"
-        image="img/icons/Headline.svg"
+        image="img/icons/Headline-dark.svg"
         topic="Lorem Ipsum"
         text="kickstartDS makes it easy to implement and use [its packages](http://example.com/) across any framework or no framework at all. We accomplish this by using standardized web platform APIs and [Web Components](http://example.com/)."
       />
       <TeaserBox
         darkStyle= "true"
-        image="img/icons/Quote.svg"
+        image="img/icons/Quote-dark.svg"
         topic="Accessibility baked in"
         text="kickstartDS comes accessibility baked in and provides support for W3C’s [WCAG 2.1 spec](http://example.com/). We make sure all our components support touch screens, keyboard navigation, screen readers, and more."
       />
       <TeaserBox
         darkStyle= "true"
+        image="img/icons/Slider-dark.svg"
+        topic="Powerful theming tools"
+        text="kickstartDS comes in two themes and allows you to choose globally or locally [per component](http://example.com/) which theme you want to utilize."
+      />
+    </Section>
+    <Section
+      gutter="default"
+      headline={{
+        align: 'center',
+        content: 'What kickstartDS can do for you?',
+        level: 'h2',
+        pageHeader: false,
+        spaceAfter: 'none'
+      }}
+      background="dark"
+      mode="tile"
+      spaceAfter="default"
+      spaceBefore="default"
+      width="default"
+    >
+      <TeaserBox
+        darkStyle= "true"
+        image="img/icons/Headline-dark.svg"
+        topic="Lorem Ipsum"
+        text="kickstartDS makes it easy to implement and use [its packages](http://example.com/) across any framework or no framework at all. We accomplish this by using standardized web platform APIs and [Web Components](http://example.com/)."
+      />
+      <TeaserBox
+        darkStyle= "true"
+        image="img/icons/Quote-dark.svg"
+        topic="Accessibility baked in"
+        text="kickstartDS comes accessibility baked in and provides support for W3C’s [WCAG 2.1 spec](http://example.com/). We make sure all our components support touch screens, keyboard navigation, screen readers, and more."
+      />
+      <TeaserBox
+        darkStyle= "true"
+        image="img/icons/Slider-dark.svg"
+        topic="Powerful theming tools"
+        text="kickstartDS comes in two themes and allows you to choose globally or locally [per component](http://example.com/) which theme you want to utilize."
+      />
+    </Section>
+    <Section
+      gutter="default"
+      headline={{
+        align: 'center',
+        content: 'What kickstartDS can do for you?',
+        level: 'h2',
+        pageHeader: false,
+        spaceAfter: 'none'
+      }}
+      background="accent"
+      mode="tile"
+      spaceAfter="default"
+      spaceBefore="default"
+      width="default"
+    >
+      <TeaserBox
+        image="img/icons/Headline.svg"
+        topic="Lorem Ipsum"
+        text="kickstartDS makes it easy to implement and use [its packages](http://example.com/) across any framework or no framework at all. We accomplish this by using standardized web platform APIs and [Web Components](http://example.com/)."
+      />
+      <TeaserBox
+        image="img/icons/Quote.svg"
+        topic="Accessibility baked in"
+        text="kickstartDS comes accessibility baked in and provides support for W3C’s [WCAG 2.1 spec](http://example.com/). We make sure all our components support touch screens, keyboard navigation, screen readers, and more."
+      />
+      <TeaserBox
         image="img/icons/Slider.svg"
         topic="Powerful theming tools"
         text="kickstartDS comes in two themes and allows you to choose globally or locally [per component](http://example.com/) which theme you want to utilize."
