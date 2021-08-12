71a740e86d804b45e5f53b48d353789a01307231
fleven <franz.leven@kickstartds.com>
2021-08-12 13:04:42 +0200

    feat: add button animation

--- src/Seiten/Landingpage.story.jsx
+++ src/Seiten/Landingpage.story.jsx
@@ -6,6 +6,7 @@ import {
   TeaserBox,
   Divider,
 } from "@kickstartds/base";
+import { Button } from "@kickstartds/base/lib/button";
 import {
   Visual,
   CollapsibleBox,
@@ -39,7 +40,7 @@ const Page = () => (
           icon: {
             icon: 'chevron-right',
           },
-          label: 'Lorem Ipsum',
+          label: 'Request a guided demo',
           newTab: false,
           size: 'medium',
           variant: 'solid'
@@ -60,6 +61,8 @@ const Page = () => (
     />
     <Section
       width="wide"
+      spaceBefore="small"
+      spaceAfter="small"
       >
       <LogoTiles
         logos={[
@@ -112,7 +115,41 @@ const Page = () => (
         text="kickstartDS comes in two themes and allows you to choose globally or locally [per component](http://example.com/) which theme you want to utilize."
       />
     </Section>
+    <Section width="wide">
+      <Section
+      headline={{
+        align: 'center',
+        content: 'Great components',
+        subheadline: 'Make the most out of your frontends by unifying their core utilizing component and design tokens',
+        level: 'h2',
+        pageHeader: false,
+        spaceAfter: 'none'
+      }}
+      mode="list"
+      className="l-section--deko l-section__pattern l-section__pattern-1"
+      width="narrow"
+      background="dark"
+      align="center"
+      >
+        <TextMedia
+          media={null}
+          mediaAlignment="above-center"
+          text='kickstart DS is the foundation for your **design system** with reusable components and tools, guided by clear standards, that can be assembled together to **build digital** products: fast, flexible, responsive, accessible, xxx, yyy, zzz and the best of all:
 
+you can plug it onto every digital touch point you own.'
+        />
+        <Button
+          className="c-button--main no-stretch"
+          href="#"
+          label="mehr erfahren"
+          size="medium"
+          type="button"
+          variant="solid-inverted"
+        />
+      </Section>
+
+    </Section>
+    
     <Section
     className="l-section__pattern l-section__pattern-1"
     >
@@ -120,6 +157,41 @@ const Page = () => (
         image="https://picsum.photos/seed/kdsquote/300/300"
         source="Max Mustermann, ABC GmbH"
         text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+      />
+    </Section>
+
+    <Section
+      gutter="default"
+      headline={{
+        align: 'center',
+        content: 'What kickstartDS can do for you?',
+        subheadline: 'kickstartDS comes accessibility baked in and provides support',
+        level: 'h2',
+        pageHeader: false,
+        spaceAfter: 'none'
+      }}
+      mode="tile"
+      spaceAfter="default"
+      spaceBefore="default"
+      width="default"
+    >
+      <TeaserBox
+        className="c-teaser--animated"
+        image="img/icons/Headline.svg"
+        topic="Lorem Ipsum"
+        text="kickstartDS makes it easy to implement and use [its packages](http://example.com/) across any framework or no framework at all. We accomplish this by using standardized web platform APIs and [Web Components](http://example.com/)."
+      />
+      <TeaserBox
+        className="c-teaser--animated"
+        image="img/icons/Quote.svg"
+        topic="Accessibility baked in"
+        text="kickstartDS comes accessibility baked in and provides support for W3C’s [WCAG 2.1 spec](http://example.com/). We make sure all our components support touch screens, keyboard navigation, screen readers, and more."
+      />
+      <TeaserBox
+        className="c-teaser--animated"
+        image="img/icons/Slider.svg"
+        topic="Powerful theming tools"
+        text="kickstartDS comes in two themes and allows you to choose globally or locally [per component](http://example.com/) which theme you want to utilize."
       />
     </Section>
 
@@ -154,37 +226,6 @@ const Page = () => (
       <ContentBox
         image="https://picsum.photos/seed/kdsteaserbox/500/300"
         ratio="16:9"
-        topic="Powerful theming tools"
-        text="kickstartDS comes in two themes and allows you to choose globally or locally [per component](http://example.com/) which theme you want to utilize."
-      />
-    </Section>
-
-    <Section
-      gutter="default"
-      headline={{
-        align: 'center',
-        content: 'What kickstartDS can do for you?',
-        level: 'h2',
-        pageHeader: false,
-        spaceAfter: 'none'
-      }}
-      mode="tile"
-      spaceAfter="default"
-      spaceBefore="default"
-      width="default"
-    >
-      <TeaserBox
-        image="img/icons/Headline.svg"
-        topic="Lorem Ipsum"
-        text="kickstartDS makes it easy to implement and use [its packages](http://example.com/) across any framework or no framework at all. We accomplish this by using standardized web platform APIs and [Web Components](http://example.com/)."
-      />
-      <TeaserBox
-        image="img/icons/Quote.svg"
-        topic="Accessibility baked in"
-        text="kickstartDS comes accessibility baked in and provides support for W3C’s [WCAG 2.1 spec](http://example.com/). We make sure all our components support touch screens, keyboard navigation, screen readers, and more."
-      />
-      <TeaserBox
-        image="img/icons/Slider.svg"
         topic="Powerful theming tools"
         text="kickstartDS comes in two themes and allows you to choose globally or locally [per component](http://example.com/) which theme you want to utilize."
       />
---
+++ src/storytelling/Storytelling.stories.jsx
@@ -0,0 +1,35 @@
+import { pack } from "@kickstartds/core/lib/storybook/helpers";
+import storytellingStories, {
+  Template,
+} from "@kickstartds/content/lib/storytelling/storytelling.stories";
+import cssprops from "./storytelling-tokens.json";
+
+export default {
+  ...storytellingStories,
+  parameters: {
+    cssprops,
+  },
+};
+
+export const Bild16zu9 = Template.bind({});
+Bild16zu9.args = pack({
+  image: {
+    source: "img/2014_studenten_55_medium1.jpg",
+  },
+  box: {
+    headline: {
+      level: "h2",
+      content: "Great components",
+      subheadline: "Make the most out of your frontends by unifying their core utilizing component and design tokens",
+    },
+    text:
+      "kickstart DS is the foundation for your design system with reusable components and tools, guided by clear standards, that can be assembled together to build digital products: fast, flexible, responsive, accessible, xxx, yyy, zzz and the best of all: you can plug it onto every digital touch point you own.",
+    link: {
+      label: "Request a guided demo",
+      href: "#",
+      variant: "solid",
+    },
+  },
+  full: true,
+});
+
