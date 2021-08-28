51c55fcbc02398e29d415c52adfb86083979667f
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-08-28 15:19:13 +0200

    fix: also publish original schemas

--- scripts/copySchema.js
+++ scripts/copySchema.js
@@ -2,7 +2,7 @@ const fs = require("fs-extra");
 const fg = require("fast-glob");
 
 (async () => {
-  const schemaPaths = await fg("src/**/*.schema.dereffed.json");
+  const schemaPaths = await fg("src/**/*.schema.(?:dereffed.)?json");
   return Promise.all(schemaPaths.map(
     (schemaPath) => {
       const dest = schemaPath.replace("src/", "dist/components/")
