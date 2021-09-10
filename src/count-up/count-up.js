d8ab63205d070dc3f785f09bb8a3feff9c93beb0
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2021-09-10 10:15:40 +0200

    fix: don't access `window` in ssg

--- src/count-up/count-up.js
+++ src/count-up/count-up.js
@@ -1,4 +1,8 @@
-window.rm.radio.on('content.count-up.start', (_, { element }) => {
-  const fill = element.closest('.kds-count-up').querySelector('.kds-count-up__fill');
-  fill && fill.classList.add('active');
-});
+import { inBrowser } from "@kickstartds/core/lib/core";
+
+if (inBrowser) {
+  window.rm.radio.on('content.count-up.start', (_, { element }) => {
+    const fill = element.closest('.kds-count-up').querySelector('.kds-count-up__fill');
+    fill && fill.classList.add('active');
+  });
+}
