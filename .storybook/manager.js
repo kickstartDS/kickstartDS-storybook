0fabea174f68715d524aa0ad5bae460e4c84d67d
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2021-09-29 12:49:20 +0200

    feat: load zendesk lazily

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,4 +1,14 @@
+import React from "react";
+import { render } from "react-dom";
 import { addons } from "@storybook/addons";
 import { light } from "./themes";
+import { zESettings } from "../src/zendesk/zESettings";
+import { IconSprite } from '../src/icon-sprite/IconSpriteComponent';
+import { ZendeskButton } from "../src/zendesk/ZendeskButtonComponent";
 
 addons.setConfig({ theme: light });
+
+window.zESettings = zESettings;
+const zendeskWrapper = document.createElement("div");
+document.body.appendChild(zendeskWrapper);
+render(<><IconSprite /><ZendeskButton /></>, zendeskWrapper);
---
+++ src/zendesk/zESettings.js
@@ -0,0 +1,82 @@
+export const zESettings = {
+  webWidget: {
+    launcher: {
+      label: {
+        'en-gb': 'Learn more',
+        'en-us': 'Learn more',
+        'de': 'Mehr erfahren',
+        '*': 'Learn more'
+      },
+      chatLabel: {
+        'en-gb': 'Learn more',
+        'en-us': 'Learn more',
+        'de': 'Mehr erfahren',
+        '*': 'Learn more'
+      }
+    },
+    contactOptions: {
+      enabled: true,
+      chatLabelOnline: {
+        'en-gb': 'Chat with us',
+        'en-us': 'Chat with us',
+        'de': 'Chatte mit uns',
+        '*': 'Chat with us'
+      },
+      chatLabelOffline: {
+        'en-gb': 'Chat currently offline',
+        'en-us': 'Chat currently offline',
+        'de': 'Chat aktuell offline',
+        '*': 'Chat currently offline'
+      },
+      contactFormLabel: {
+        'en-gb': 'Send us a message',
+        'en-us': 'Send us a message',
+        'de': 'Sende uns eine Nachricht',
+        '*': 'Send us a message'
+      },
+      title: {
+        'en-gb': 'Learn more',
+        'en-us': 'Learn more',
+        'de': 'Mehr erfahren',
+        '*': 'Learn more'
+      },
+    },
+    contactForm: {
+      attachments: false,
+      subject: false,
+      title: {
+        'en-gb': 'Learn more',
+        'en-us': 'Learn more',
+        'de': 'Mehr erfahren',
+        '*': 'Learn more'
+      },
+      selectTicketForm: {
+        'en-gb': 'How can we best help you:',
+        'en-us': 'How can we best help you:',
+        'de': 'Wir können wir dir am besten helfen:',
+        '*': 'How can we best help you:'
+      }
+    },
+    chat: {
+      departments: {
+        enabled: ['kickstartDS']
+      },
+      concierge: {
+        avatarPath: 'https://preview.kickstartds.com/images/kickstartds-some_640x640.png',
+        name: 'kickstartDS Team',
+        title: {
+          'en-gb': '... connect with us 😊',
+          'en-us': '... connect with us 😊',
+          'de': '... nimm Kontakt mit uns auf 😊',
+          '*': '... connect with us 😊'
+        }
+      },
+      title: {
+        'en-gb': 'Learn more',
+        'en-us': 'Learn more',
+        'de': 'Mehr erfahren',
+        '*': 'Learn more'
+      }
+    }
+  }
+};
