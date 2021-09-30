2d2479b96cb56dcba44b754d22c13918f4823f40
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-09-30 20:37:52 +0200

    fix: attempt to fix window issue

--- src/zendesk/ZendeskButtonComponent.jsx
+++ src/zendesk/ZendeskButtonComponent.jsx
@@ -5,36 +5,41 @@ import { zESettings } from "./zESettings";
 
 const { defineProperty } = Object;
 const waitForZendesk = () => {
-  const promise = new Promise((resolve) => {
-    if (window.zE) {
-      if (typeof window.zE === "function") {
-        return resolve(window.zE);
+  if (inBrowser) {
+    const promise = new Promise((resolve) => {
+      if (window.zE) {
+        if (typeof window.zE === "function") {
+          return resolve(window.zE);
+        }
+        return window.zE.then(resolve);
       }
-      return window.zE.then(resolve);
-    }
-
-    defineProperty(window, "zE", {
-      configurable: true,
-      get() {
-        return promise;
-      },
-      set(value) {
-        defineProperty(window, "zE", {
-          configurable: true,
-          enumerable: true,
-          writable: true,
-          value,
-        });
-        resolve(window.zE);
-      },
+  
+      defineProperty(window, "zE", {
+        configurable: true,
+        get() {
+          return promise;
+        },
+        set(value) {
+          defineProperty(window, "zE", {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value,
+          });
+          resolve(window.zE);
+        },
+      });
+      const script = document.createElement("script");
+      script.setAttribute("id", "ze-snippet");
+      script.src =
+        "https://static.zdassets.com/ekr/snippet.js?key=7f767236-672e-4632-997e-310b62ab340b";
+      document.body.appendChild(script);
     });
-    const script = document.createElement("script");
-    script.setAttribute("id", "ze-snippet");
-    script.src =
-      "https://static.zdassets.com/ekr/snippet.js?key=7f767236-672e-4632-997e-310b62ab340b";
-    document.body.appendChild(script);
-  });
-  return promise;
+
+    return promise;
+  }
+
+  return Promise.resolve();
 };
 
 const activate = () => window.zE.activate();
