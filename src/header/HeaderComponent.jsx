c4bcb1275ba3b527f08a34b63dfba342f572ddc0
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-10-01 22:01:07 +0200

    fix: add alt text to logo

--- src/header/HeaderComponent.jsx
+++ src/header/HeaderComponent.jsx
@@ -5,7 +5,7 @@ export const Header = () => (
     <header data-component="kds.header">
       <div className="kds-header">
         <Link className="kds-header__logo" href="/">
-          <img src="/logo.svg" />
+          <img src="/logo.svg" alt="kickstartDS Logo" />
         </Link>
         <div className="kds-header__item">
           <Link className="kds-header__link c-button c-button--small c-button--outline" href="/blog">
