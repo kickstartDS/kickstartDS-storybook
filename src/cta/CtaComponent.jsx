07457e75c4bc62d734fc5344e36544dc6eecb877
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2021-10-29 15:03:20 +0200

    feat: set default props for cta component

--- src/cta/CtaComponent.jsx
+++ src/cta/CtaComponent.jsx
@@ -1,13 +1,28 @@
 import { Headline } from "@kickstartds/base/lib/headline";
 import { Storytelling } from "@kickstartds/content/lib/storytelling";
+import { LinkButton } from "@kickstartds/base/lib/link-button";
 
-export const Cta = ({headline, storytelling}) => (
+export const Cta = ({
+  headline: headlineProps,
+  storytelling: storytellingProps,
+  button,
+}) => {
+  const { box, image, ...storytelling } = storytellingProps;
+  const { headline, link, ...customBox } = box;
+  const { order, ...customImage } = image;
+  const { desktopImageLast, ...customOrder } = customImage;
+  return (
     <div className="cta">
-        <Headline
-            {...headline}
-        />
-        <Storytelling
-            {...storytelling}
-        />
+      <Headline {...headlineProps} align={headlineProps.align || "left"} />
+      <Storytelling
+        {...storytelling}
+        box={customBox}
+        image={{
+          ...customImage,
+          order: { ...customOrder, desktopImageLast: desktopImageLast || true },
+        }}
+      />
+      <LinkButton {...button} />
     </div>
-)
+  );
+};
