e8b033a177f5c8fb0ae24e1222ecde703c21dce6
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2022-04-11 20:09:17 +0200

    docs: fix internal storybook links

--- docs/LinkProvider.jsx
+++ docs/LinkProvider.jsx
@@ -2,7 +2,7 @@ import { LinkContext, LinkContextDefault } from "@kickstartds/base/lib/link";
 import LinkTo from "@storybook/addon-links/react";
 
 const StorybookLink = ({ href, ...props }) => {
-  if (href.startsWith("sb:")) {
+  if (href?.startsWith("sb:")) {
     const [, kind, story] = href.split(":");
     return <LinkTo kind={kind} story={story} {...props} />;
   }
