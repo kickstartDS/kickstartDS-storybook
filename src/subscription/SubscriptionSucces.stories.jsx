36eb631d9be0802e5db960612322c7ac07d9d7f8
fleven <franz.leven@kickstartds.com>
2022-04-13 13:28:05 +0200

    feat: post adjustments + add subscription succes

---
+++ src/subscription/SubscriptionSucces.stories.jsx
@@ -0,0 +1,10 @@
+import { SubscriptionSucces } from "./SubscriptionSuccesComponent";
+
+const Template = (args) => <SubscriptionSucces {...args} />;
+
+export default {
+  title: "Form/Subscription",
+  component: SubscriptionSucces,
+};
+
+export const Succes = Template.bind({});
---
+++ src/subscription/SubscriptionSuccesComponent.tsx
@@ -0,0 +1,9 @@
+import { RichText } from "@kickstartds/base/lib/rich-text";
+import { TextField } from "@kickstartds/form/lib/text-field";
+import { Button } from "@kickstartds/base/lib/button";
+
+export const SubscriptionSucces = ({}) => (
+  <>
+    <RichText text="😇 Thank You! We will get back to you as soon as possible" />
+  </>
+);
