4495067373260073a3627f8e6a3451268a78de59
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2022-04-25 22:40:18 +0200

    fix: remove legacy rm resolver for good

--- scripts/schemaResolver.js
+++ scripts/schemaResolver.js
@@ -1,20 +1,6 @@
 const fs = require("fs-extra");
 const glob = require("fast-glob");
 
-const rmUrlRegExp =
-  /^http:\/\/frontend\.ruhmesmeile\.com\/([a-z-_]+)\/([a-z-_]+)\/([a-z-_/]+)\.(?:schema|definitions)\.json$/;
-
-const rmResolver = {
-  canRead: /^http:\/\/frontend\.ruhmesmeile\.com/i,
-  async read(file) {
-    const [, module, , name] = rmUrlRegExp.exec(file.url);
-    const [resolvedPath] = await glob(
-      `node_modules/@kickstartds/${module}/lib/**/${name}.(schema|definitions).json`
-    );
-    return fs.readJSON(resolvedPath);
-  },
-};
-
 const kdsUrlRegExp =
   /^http:\/\/schema\.kickstartds\.com\/([a-z-_]+)\/([a-z-_/]+)\.(?:schema|definitions)\.json$/i;
 
@@ -43,4 +29,4 @@ const kickstartdsResolver = {
   },
 };
 
-module.exports = { rmResolver, kdsResolver, kickstartdsResolver };
+module.exports = { kdsResolver, kickstartdsResolver };
