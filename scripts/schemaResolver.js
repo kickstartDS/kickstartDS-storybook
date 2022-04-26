e8f592a3e170e25f3b7850a59cd226307cb9920d
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2022-04-26 23:03:14 +0200

    feature: upgrade kickstartDS core deps

--- scripts/schemaResolver.js
+++ scripts/schemaResolver.js
@@ -1,29 +1,15 @@
 const fs = require("fs-extra");
 const glob = require("fast-glob");
 
-const rmUrlRegExp =
-  /^http:\/\/frontend\.ruhmesmeile\.com\/([a-z-_]+)\/([a-z-_]+)\/([a-z-_/]+)\.(?:schema|definitions)\.json$/;
-
-const rmResolver = {
-  canRead: /^http:\/\/frontend\.ruhmesmeile\.com/i,
-  async read(file) {
-    const [, module, , name] = rmUrlRegExp.exec(file.url);
-    const [resolvedPath] = await glob(
-      `node_modules/@kickstartds/${module}/lib/**/${name}.(schema|definitions).json`
-    );
-    return fs.readJSON(resolvedPath);
-  },
-};
-
 const kdsUrlRegExp =
   /^http:\/\/schema\.kickstartds\.com\/([a-z-_]+)\/([a-z-_/]+)\.(?:schema|definitions)\.json$/i;
 
 const kdsResolver = {
-  canRead: kdsUrlRegExp,
+  canRead: /^http:\/\/schema\.kickstartds\.com/i,
   async read(file) {
     const [, module, name] = kdsUrlRegExp.exec(file.url);
     const [resolvedPath] = await glob(
-      `node_modules/@kickstartds/${module}/lib/${name}/${name}.(schema|definitions).json`
+      `node_modules/@kickstartds/${module}/lib/${name === 'lightbox-lazy-image' ? 'lightbox-image' : name}/${name}.(schema|definitions).json`
     );
     return fs.readJSON(resolvedPath);
   },
@@ -43,4 +29,4 @@ const kickstartdsResolver = {
   },
 };
 
-module.exports = { rmResolver, kdsResolver, kickstartdsResolver };
+module.exports = { kdsResolver, kickstartdsResolver };
