416108f2e45fe12b7c37c66d982b423876e0f3ff
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2022-09-28 16:19:59 +0200

    fix: fix session storage hook

--- src/hooks/useSessionState.tsx
+++ src/hooks/useSessionState.tsx
@@ -1,21 +1,28 @@
 import {
   useEffect,
   useState,
-  useContext,
-  createContext,
+  useCallback,
   Dispatch,
   SetStateAction,
 } from "react";
 
-const SessionStateContext = createContext((sessionKey: string): [
-  string | null,
-  Dispatch<SetStateAction<string | null>>
-] => {
+export const useSessionState = (
+  sessionKey: string
+): [string | null, Dispatch<SetStateAction<string | null>>] => {
   if (typeof window === "undefined") {
     return [null, () => {}];
   }
 
   const [value, setValue] = useState(sessionStorage.getItem(sessionKey));
+  const updateValue = useCallback(() => {
+    setValue(sessionStorage.getItem(sessionKey));
+  }, [sessionKey]);
+
+  useEffect(() => {
+    updateValue();
+    window.addEventListener("session-storage", updateValue);
+    return () => window.removeEventListener("session-storage", updateValue);
+  }, [updateValue]);
 
   useEffect(() => {
     if (value == null) {
@@ -23,9 +30,8 @@ const SessionStateContext = createContext((sessionKey: string): [
     } else {
       sessionStorage.setItem(sessionKey, value);
     }
+    window.dispatchEvent(new Event("session-storage"));
   }, [value]);
-  return [value, setValue];
-});
 
-export const useSessionState = (sessionKey: string) =>
-  useContext(SessionStateContext)(sessionKey);
+  return [value, setValue];
+};
