a7dd67f82ba1d8b3e96cb7dd4a49e5b52e1b0b6e
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2022-10-21 11:24:06 +0200

    feat: add bedrock

--- src/Providers.jsx
+++ src/Providers.jsx
@@ -5,19 +5,22 @@ import { SectionProvider } from "./section/SectionComponent";
 import { CountUpProvider } from "./count-up/CountUpComponent";
 import { StorytellingProvider } from "./storytelling/StorytellingComponent";
 import { VisualProvider } from "./visual/VisualComponent";
+import { BedrockProvider } from "./bedrock/BedrockProvider";
 
 export default (props) => (
-  <HeadlineProvider>
-    <ButtonProvider>
-      <LinkButtonProvider>
-        <SectionProvider>
-          <StorytellingProvider>
-            <VisualProvider>
-              <CountUpProvider {...props} />
-            </VisualProvider>
-          </StorytellingProvider>
-        </SectionProvider>
-      </LinkButtonProvider>
-    </ButtonProvider>
-  </HeadlineProvider>
+  <BedrockProvider>
+    <HeadlineProvider>
+      <ButtonProvider>
+        <LinkButtonProvider>
+          <SectionProvider>
+            <StorytellingProvider>
+              <VisualProvider>
+                <CountUpProvider {...props} />
+              </VisualProvider>
+            </StorytellingProvider>
+          </SectionProvider>
+        </LinkButtonProvider>
+      </ButtonProvider>
+    </HeadlineProvider>
+  </BedrockProvider>
 );
