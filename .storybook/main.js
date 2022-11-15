bc88a2d221fbc41cf658b44c974d1a9ead123001
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2022-11-15 16:59:55 +0100

    feat: add Storybook addon-a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
   ],
   addons: [
     "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
     "@kickstartds/storybook-addon-component-tokens",
     "@whitespace/storybook-addon-html",
     "@kickstartds/storybook-addon-jsonschema",
