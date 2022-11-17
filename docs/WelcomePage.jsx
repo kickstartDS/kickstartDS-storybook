b5419b8f2c94193e3a06165dcdf269fbad928f1e
fleven <franz.leven@kickstartds.com>
2022-11-17 14:39:56 +0100

    feat(welcome page): remove product hunt banner

--- docs/WelcomePage.jsx
+++ docs/WelcomePage.jsx
@@ -57,36 +57,6 @@ export const Welcome = () => (
     </Section>
 
     <Section
-      spaceBefore="small"
-      spaceAfter="small"
-      width="wide"
-      background="accent"
-      mode="list"
-      align="center"
-      gutter="none"
-      headline={{
-        level: "h2",
-        styleAs: "h3",
-        content: "We are also live on Product Hunt",
-        subheadline: "Learn how to own a Design System without investing years of development.",
-        align: "center",
-      }}
-    >
-      <div>
-        <a
-          href="https://www.producthunt.com/posts/kickstartds?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-kickstartds"
-          target="_blank"
-        >
-          <img
-            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=366575&theme=light"
-            alt="kickstartDS is live on Product Hunt"
-            width="250"
-            height="54"
-          />
-        </a>
-      </div>
-    </Section>
-    <Section
       headline={{
         align: "center",
         content: "Getting started with ...",
