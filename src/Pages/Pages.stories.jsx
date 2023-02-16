2e0c22926eb0a34b4119f1b8ff97e845bad229e8
fleven <franz.leven@kickstartds.com>
2023-02-16 10:57:47 +0700

    feat(pages): add example content

--- src/Pages/Pages.stories.jsx
+++ src/Pages/Pages.stories.jsx
@@ -36,5 +36,7 @@ export { BlogPost } from "./BlogPost.story";
 export { BlogList } from "./BlogList.story";
 export { Buttons } from "./Buttons.story";
 export { ShowcaseEntry } from "./ShowcaseEntry.story";
+// export { MaxclusterShowcase } from "./MaxclusterShowcase.story";
+// export { EncoreShowcase } from "./EncoreShowcase.story";
 export { AppearanceEntry } from "./AppearanceEntry.story";
 export { GlossaryEntry } from "./GlossaryEntry.story";
