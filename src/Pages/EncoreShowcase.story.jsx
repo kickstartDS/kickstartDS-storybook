4de531921c53823ead678db76b95c0398da1fd1c
fleven <franz.leven@kickstartds.com>
2023-02-17 12:58:18 +0700

    feat(encoeShowcase): add encore showcase content

--- src/Pages/EncoreShowcase.story.jsx
+++ src/Pages/EncoreShowcase.story.jsx
@@ -19,32 +19,37 @@ const Page = () => (
         caption: 'Encore Logo',
         src: 'img/showcase/encore/encore-cover.png'
       }}
+      author={{
+        name: "Daniel Ley",
+        avatar: "img/profile_images_dley.webp"
+      }}
+      summary="encore as a German utility company."
       description="encore as a German utility company supports their customers with modern, cloud-based software solutions, and a wide spectrum of services around their business. They create online experiences for their customers, and future-proof their eCommerce, energy billing and services portfolio."
       link="https://www.encore.de/"
       media={[
         {
-          caption: 'Dynamic wheel carousel',
-          src: 'img/showcase/encore/encore-wheel.png'
+          src: 'img/showcase/encore/encore-wheel.gif',
+          caption: 'Dynamic wheel carousel'
         },
         {
-          caption: 'Showcase screenshot',
-          src: 'img/showcase/encore/encore-slider.png'
+          src: 'img/showcase/encore/encore-slider.png',
+          caption: 'Showcase screenshot'
         },
         {
-          caption: 'Showcase screenshot',
-          src: 'img/showcase/Showcase_screen.png'
-        }
+          src: 'img/showcase/encore/encore-paralax.gif',
+          caption: 'We created a paralax effect for the floating text with multiple background layers.'
+        },
+
       ]}
       quote={{
         byline: 'CEO',
         className: undefined,
-        image: 'https://picsum.photos/seed/kdsquote/300/300',
+        image: 'img/showcase/encore/encore-quote.jpeg',
         quoteToggle: true,
         source: 'Richard McClintock',
         text: 'Consequuntur est quia atque eum ducimus. Consequatur fugiat hic eos perferendis. Libero unde omnis dolorem maxime occaecati sapiente distinctio. Consectetur non placeat dolorem est animi.\n\nSuscipit quos rem suscipit culpa. Et distinctio et veritatis consectetur quae dolorem aut. Explicabo commodi repudiandae fugiat. Accusamus alias mollitia perferendis qui iure magni. Minus est vitae dolor dolor.'
       }}
       related={[
-
         {
           excerpt: 'Komplexe Technologien für Unternehmen unkompliziert nutzbar machen.',
           image: 'img/showcase/maxcluster.jpeg',
